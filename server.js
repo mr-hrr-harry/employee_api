1d00beafedfb39cc92f00621d5b60fef3faad33c
M A HARISH J <mr.horror3737@gmail.com>
2023-08-02 22:42:04 +0530

    server started successfully

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express')
 const app = express()
+
 require('dotenv').config
+require('./database/dbConnect')
+
+app.use(express.json())
+
+app.listen(process.env.PORT, ()=> {
+    console.log("Server Started!");
+})
